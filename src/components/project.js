94213107c480321e62769e2bcaa067532c4b5beb
Justin Pickens <justin.pickens@gmx.com>
2021-09-23 14:12:13 -0500

    Created Project component

--- src/components/project.js
+++ src/components/project.js
@@ -1,10 +1,52 @@
 import React from 'react';
 
-export default function Project () {
+export default function Project(props) {
 
+    // Make props more legible
+    const projectData = props;
+
+    // Define card style dependent on isPrimary
+    const headerStyle = projectData.isPrimary?
+        { // Yep
+            classList: "card shiny topleft",
+            styleMod: { marginTop: 0 },
+        }
+        :
+        { // Nope
+            classList: "card topleft",
+            styleMod: {},
+        };
+
+    /*
+    Props:
+        isPrimary - top of page
+        projectTitle - Name of project
+        projectGithub - project repository
+        projectDeployed - demo of project
+        projectScreenshot - src of project screenshot
+        projectScreenshotDesc - alt text
+    */
 
     return (
         <>
+            <div className={headerStyle.classList} style={headerStyle.styleMod}>
+                <div className="cardHeader">
+                    <h3>{projectData.projectTitle}</h3>
+                    <a
+                        href={projectData.projectGithub}
+                        target="_blank"
+                        rel="noreferrer"
+                    >Github Repo</a>
+                </div>
+                <a
+                    href={projectData.projectDeployed}
+                    target="_blank"
+                    rel="noreferrer">
+                    <img src={projectData.projectScreenshot}
+                        alt={projectData.projectScreenshotDesc}
+                    />
+                </a>
+            </div>
         </>
     );
 }
